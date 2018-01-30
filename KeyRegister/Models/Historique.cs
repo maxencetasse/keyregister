b42487988b90ec8415c2c5f0b560d190ad1dd996
Virginie LOISEL <285334@supinfo.com>
2018-01-30 19:36:05 +0100

    ajout des modèles : Campus, Historique

---
+++ KeyRegister/Models/Historique.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace KeyRegister.Models
+{
+    public class Historique
+    {
+        public int idHistorique { get; set; }
+        public int idBootser { get; set; }
+        public int idReservation { get; set; }
+        public int idCampus { get; set; }
+        public int idEtat { get; set; }
+        public DateTime dateHistorique { get; set; }
+    }
+}
