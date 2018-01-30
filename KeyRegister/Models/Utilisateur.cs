ca301daf5d8ebd48c347abaf79636b8e70382e14
VASSARD Oweun <213559@supinfo.com>
2018-01-30 19:34:58 +0100

    Ajout des models (catégorie, etat, reservation, role, utilisateur)

---
+++ KeyRegister/Models/Utilisateur.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace KeyRegister.Models
+{
+    public class Utilisateur
+    {
+        public int idBooster { get; set; }
+        public string nomUtilisateur { get; set; }
+        public string prenomUtilisateur { get; set; }
+        public int idRole { get; set; }
+        public int idCampus { get; set; }
+        public DateTime dateInscriptionUtilisateur { get; set; }
+        public bool charteUtilisateur { get; set; }
+    }
+}
